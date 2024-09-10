e1be7f9dd0b2fdc80df8760ce1253103afd35aeb
Thayna Pereira <thaynamendess997557@gmail.com>
2024-09-10 16:01:35 -0300

    cadastar meta

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const{ select } = require('@inquirer/prompts')
+const{ select, input } = require('@inquirer/prompts')
+let meta = {
+    value: 'Tomar 3L de agua',
+    checked: false,
+}
+let metas = [meta]
+
+const cadastrarMeta = async () =>{
+    const meta = await  input({message: 'Digite a meta:'})
+
+    if(meta.length == 0){
+        console.log('A meta não pode ser vazia.')
+        return 
+    
+    }
+
+    metas.push(
+        { value: meta, checked: false}
+    )
+
+
+}
 
 const start = async () =>{
 
@@ -23,7 +44,8 @@ const start = async () =>{
 
         switch(opcao){
             case 'cadastrar':
-                console.log('vamos cadastrar')
+                await cadastrarMeta()
+                console.log(metas)
                 break
             
             case 'listar':
