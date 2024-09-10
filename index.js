71c3bdab720bc567dbf9ba9c1753fa1ad11e0d4e
Thayna Pereira <thaynamendess997557@gmail.com>
2024-09-10 15:31:22 -0300

    menu aplicação

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const start = () =>{
+const{ select } = require('@inquirer/prompts')
+
+const start = async () =>{
 
     while(true){
-        let opcao = 'sair'
+        const opcao = await select({
+            message: 'Menu >',
+            choices: [
+                {
+                    name: 'cadastrar meta',
+                    value: 'cadastrar'
+                },
+                {
+                    name: 'sair',
+                    value: 'sair'
+                },
+                {
+                    name: 'Listar metas',
+                    value: 'listar'
+                },
+            ]
+        })
 
         switch(opcao){
             case 'cadastrar':
@@ -13,11 +31,12 @@ const start = () =>{
                 break
             
             case 'sair':
+                console.log('Até a próxima')
                 return
             
         }
         
-        return
+        
     }
     
 }
