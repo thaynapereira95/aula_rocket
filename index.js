5be1aed92885281f15f25f0f8c67aac09dae4686
Thayna Pereira <thaynamendess997557@gmail.com>
2024-09-10 16:31:10 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const{ select, input } = require('@inquirer/prompts')
+const{ select, input, checkbox } = require('@inquirer/prompts')
 let meta = {
     value: 'Tomar 3L de agua',
     checked: false,
@@ -19,6 +19,33 @@ const cadastrarMeta = async () =>{
     )
 
 
+}
+
+const listarMetas = async () =>{
+    const respostas = await checkbox({
+        message: 'Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa',
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0){
+        console.log('nenhuma meta selecionada')
+        return
+    }
+
+    metas.forEach((m)=>{
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
 }
 
 const start = async () =>{
@@ -49,7 +76,7 @@ const start = async () =>{
                 break
             
             case 'listar':
-                console.log('vamos listar')
+                await listarMetas()
                 break
             
             case 'sair':
@@ -60,6 +87,6 @@ const start = async () =>{
         
         
     }
-    
+     
 }
 start();
