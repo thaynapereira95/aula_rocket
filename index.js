e9cb262d400e4d3a089edd864bd4f6c11c16a8a0
Thayna Pereira <thaynamendess997557@gmail.com>
2024-09-11 14:47:58 -0300

    resolve problems

--- index.js
+++ index.js
@@ -28,14 +28,15 @@ const listarMetas = async () =>{
         instructions: false,
     })
 
+    metas.forEach((m)=>{
+        m.checked = false
+    })
+
     if(respostas.length == 0){
         console.log('nenhuma meta selecionada')
         return
     }
 
-    metas.forEach((m)=>{
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
@@ -54,7 +55,7 @@ const metasRealizadas = async () =>{
     })
 
     if(realizadas.length == 0) {
-        console.log('Não existem metas realizadas :(')
+        console.log('Não existem metas realizadas')
         return
     }
 
